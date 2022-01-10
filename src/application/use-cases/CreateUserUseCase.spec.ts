2cec227bf069281fc661d44a495169a468866439
Marlon Savian <marlon.savian@mercadofavo.com>
2022-01-10 19:11:05 -0300

    test: correct create user use case test

--- src/application/use-cases/CreateUserUseCase.spec.ts
+++ src/application/use-cases/CreateUserUseCase.spec.ts
@@ -1,19 +1,3 @@
-// import { ICreateUserUseCase } from '@/core/interfaces/ICreateUserUseCase';
-// import { IUserRepo } from '@/core/interfaces/IUserRepo';
-// import { User, UserDto } from '@/core/types/User';
-
-// // https://github.com/epratik/lambda-cleanarchitecture/tree/main/src/application/use_cases
-
-// export class CreateUserUseCase implements ICreateUserUseCase {
-//   constructor(private userRepo: IUserRepo) {
-//     this.userRepo = userRepo;
-//   }
-
-//   async execute(user: UserDto): Promise<User> {
-//     return this.userRepo.createUser(user);
-//   }
-// }
-
 import { mock } from 'jest-mock-extended';
 import { UserDto } from '@/core/types/User';
 import { IUserRepo } from '@/core/interfaces/IUserRepo';
@@ -21,11 +5,14 @@ import { CreateUserUseCase } from './CreateUserUseCase';
 
 const mockUserDto = mock<UserDto>();
 const mockUserRepo = mock<IUserRepo>();
-mockUserRepo.createUser.mockReturnValueOnce({
-  id: '123',
-  email: mockUserDto.email,
-  password: mockUserDto.password,
-});
+
+mockUserRepo.createUser.mockImplementation(() =>
+  Promise.resolve({
+    id: '123',
+    email: mockUserDto.email,
+    password: mockUserDto.password,
+  }),
+);
 
 describe('CreateUserUseCase', () => {
   describe('Given a user DTO', () => {
